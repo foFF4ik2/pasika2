315f7527647539f9bf43cb864a574f769204a178
Volodymyr <foFF4ik2@gmail.com>
2023-01-28 13:53:22 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
-// Empty JS for your own code to be here
+// Получить кнопку:
+mybutton = document.getElementById("myBtn");
+
+// Когда пользователь прокручивает вниз 120px от верхней части документа, покажите кнопку
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// Когда пользователь нажимает на кнопку, прокрутите до верхней части документа
+function topFunction() {
+  document.body.scrollTop = 0; // Для Safari
+  document.documentElement.scrollTop = 0; // Для Chrome, Firefox, IE и Opera
+}
